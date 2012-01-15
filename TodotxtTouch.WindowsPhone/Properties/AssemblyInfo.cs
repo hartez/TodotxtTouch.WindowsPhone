635491ddec5a5b23611c8b701282a53f273fe3f2
E.Z. Hart <hartez@gmail.com>
2012-01-15 11:05:40 -0700

    Updated copyright year

--- TodotxtTouch.WindowsPhone/Properties/AssemblyInfo.cs
+++ TodotxtTouch.WindowsPhone/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Traceur LLC")]
 [assembly: AssemblyProduct("TodotxtTouch.WindowsPhone")]
-[assembly: AssemblyCopyright("Copyright © Traceur LLC 2011")]
+[assembly: AssemblyCopyright("Copyright © Traceur LLC 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
